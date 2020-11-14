4cfcd0701444e65f54a6f6c8275ddaddd71b78c0
lzlend <1522180192@qq.com>
2020-11-14 19:52:58 +0800

    Switch

---
+++ src/java/SwitchTest.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SwitchTest {
+    public static void main(String[] args) {
+        //一周的计划安排
+        //周一：读书
+        //周二：读书
+        //周三：唱歌
+        //周四：读书
+        //周五：读书
+        //周六：爬山
+        //周日：游泳
+        System.out.println("这是一个简单的一周计划安排小程序");
+        System.out.println("如果你忘记了当天的计划输入今天是星期几就知道了");
+        System.out.println("------------------------------------------");
+
+    }
+}
