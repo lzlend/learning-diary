26ff3f8ade4fc52bb85a0e8f06e550da3a49cf1f
lzlend <1522180192@qq.com>
2020-11-15 20:11:21 +0800

    Switch

--- src/java/SwitchTest.java
+++ src/java/SwitchTest.java
@@ -12,6 +12,34 @@ public class SwitchTest {
         System.out.println("这是一个简单的一周计划安排小程序");
         System.out.println("如果你忘记了当天的计划输入今天是星期几就知道了");
         System.out.println("------------------------------------------");
-
+        System.out.println("请输入今天是星期几，用1~7表示：");
+        Scanner sc = new Scanner(System.in);
+        int week = sc.nextInt();
+        switch(week){
+            case 1:
+                System.out.println("今天的计划是：读书");
+                break;
+            case 2:
+                System.out.println("今天的计划是：读书");
+                break;
+                //case穿透现象
+            case 3:
+                System.out.println("今天的计划是：唱歌");
+                //break;
+            case 4:
+                System.out.println("今天的计划是：读书");
+                break;
+            case 5:
+                System.out.println("今天的计划是：读书");
+                break;
+            case 6:
+                System.out.println("今天的计划是：爬山");
+                break;
+            case 7:
+                System.out.println("今天的计划是：游泳");
+                break;
+            default:
+                System.out.println("您输入的星期有误，请重新输入！");
+        }
     }
 }
