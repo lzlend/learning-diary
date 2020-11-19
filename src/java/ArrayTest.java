84cb5d5786b377b558a81a765d0cc7f9b15f15d0
lzlend <1522180192@qq.com>
2020-11-19 20:58:31 +0800

    Array

--- src/java/ArrayTest.java
+++ src/java/ArrayTest.java
@@ -6,28 +6,32 @@ public class ArrayTest {
 
         int[] arr = new int[] {11,22,33,44,55,66,77,88,99};
         //遍历数组元素
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.println("数组中的第"+(i+1)+"个元素为："+arr[i]);
-//        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("数组中的第"+(i+1)+"个元素为："+arr[i]);
+        }
         //foreach
+        int m = 0;
         for (int k : arr) {
-            String s = k + ",";
-            System.out.print(s);
+            m = m + 1;
+//            m++;
+            System.out.println("数组中的第"+m+"个元素为："+k);
         }
+        String s = "";
         for (int n : arr) {
-            String s1 = n + ",";
-            System.out.println(s1);
+            s = s + n + ",";
         }
+        System.out.print(s.substring(0, s.length()-1));
         System.out.println();
         //找出数组元素的最大值
-        int[] arr1 = new int[] {2,5,6,1,3,7,9,8,0,4};
-        int max = arr1[0];
-        for (int j = 0; j < arr1.length; j++) {
-            if(arr1[j] > max) {
-                max = arr1[j];
+        int[] arr1 = new int[] {2, 5, 6, 1, 3, 7, 9, 8, 0, 4};
+        // l表示数组中的最大值的下标
+        int l = 0;
+        for (int j = 1; j < arr1.length; j++) {
+            if(arr1[j] > arr1[l]) {
+                l = j;
             }
         }
-        System.out.println("此数组中的最大值为："+max);
+        System.out.println("此数组中的最大值为："+arr1[l]+"，是第"+(l + 1)+"个元素");
     }
 
 }
